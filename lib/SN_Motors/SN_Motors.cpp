a4630e8c1fbaeb29691d49b7123cab1fb38c853d
Salman Naveed <st186484@stud.uni-stuttgart.de>
2025-02-01 20:58:55 +0100

    feat: added BDC MCPWM function templates

--- lib/SN_Motors/SN_Motors.cpp
+++ lib/SN_Motors/SN_Motors.cpp
@@ -1,5 +1,73 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_attr.h"
+
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
+
 #include <SN_Motors.h>
 #include <SN_Logger.h>
 #include <SN_XR_Board_Types.h>
 
 // Motor Control
+
+void SN_Motors__GPIO_Init(){
+    // Initialize Motor Control
+
+    // printf("initializing mcpwm gpio...\n");
+    // mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, GPIO_PWM0A_OUT);
+    // mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, GPIO_PWM0B_OUT);
+
+    logMessage(true, "SN_Motors_Init", "Motor Control Initialized");
+}
+
+
+
+
+
+
+static void SN_Motors_MCPWM_Init(void)
+{
+    // initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm...\n");
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 1000;    //frequency = 500Hz,
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+
+}
+
+
+/**
+ * @brief motor moves in forward direction, with duty cycle = duty %
+ */
+static void SN_Motors_Drive_Forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+{
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
+    mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, duty_cycle);
+    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_A, MCPWM_DUTY_MODE_0); //call this each time, if operator was previously in low/high state
+}
+
+/**
+ * @brief motor moves in backward direction, with duty cycle = duty %
+ */
+static void SN_Motors_Drive_Backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+{
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);
+    mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, duty_cycle);
+    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_B, MCPWM_DUTY_MODE_0);  //call this each time, if operator was previously in low/high state
+}
+
+/**
+ * @brief motor stop
+ */
+static void SN_Motors_Stop(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num)
+{
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
+}
--- lib/SN_Motors/SN_Motors.h
+++ lib/SN_Motors/SN_Motors.h
@@ -1,16 +1,32 @@
-#include <stdio.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_attr.h"
-
-#include "driver/mcpwm.h"
-#include "soc/mcpwm_periph.h"
 
 // Pin Definitions
 
-void SN_Motors__GPIO_Init();
-void SN_Motors__MCPWM_Init();
+// #define GPIO_PWM0A_OUT 15   //Set GPIO 15 as PWM0A
+// #define GPIO_PWM0B_OUT 16   //Set GPIO 16 as PWM0B
 
-void 
+#define MOTOR_A_IN1 32
+#define MOTOR_A_IN2 33
+
+#define MOTOR_B_IN1 25
+#define MOTOR_B_IN2 26
+
+#define MOTOR_C_IN1 27
+#define MOTOR_C_IN2 14
+
+#define MOTOR_D_IN1 12
+#define MOTOR_D_IN2 13
+
+#define MOTOR_A_EN 15
+#define MOTOR_B_EN 2
+#define MOTOR_C_EN 4
+#define MOTOR_D_EN 16
+
+
+static void SN_Motors__GPIO_Init(void);
+static void SN_Motors_MCPWM_Init(void);
+
+static void SN_Motors_Drive_Forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle);
+static void SN_Motors_Drive_Backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle);
+static void SN_Motors_Stop(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num);
 
