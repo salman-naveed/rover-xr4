dd5f5e56aae756be3ff08d4e28cf2bb89eb2fbde
Salman Naveed <st186484@stud.uni-stuttgart.de>
2025-02-01 21:01:06 +0100

    feat: added global system context struct and flags for executable telecommands

---
+++ lib/SN_Common/SN_Common.cpp
@@ -0,0 +1,10 @@
+#include <SN_Common.h>
+
+xr4_system_context_t xr4_system_context;
+
+
+bool EMERGENCY_STOP_ACTIVE = false;
+bool ARMED = false;
+bool HEADLIGHTS_ON = false;
+
+bool ERROR_EVENT = false;
--- lib/SN_Common/SN_Common.h
+++ lib/SN_Common/SN_Common.h
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 
 typedef struct xr4_system_context {
+    // OBC-specific fields (Telemetry, OBC -> CTU)
     float GPS_lat;
     float GPS_lon;
     float GPS_time;
@@ -17,21 +18,23 @@ typedef struct xr4_system_context {
     float Main_Bus_V;
     float Main_Bus_I;
     float temp;
-    uint8_t OBC_RSSI;
-    uint8_t Command;
-    uint8_t Comm_Mode;
-    uint8_t Joystick_X;     // Joystick raw ADC X axis value
-    uint8_t Joystick_Y;     // Joystick raw ADC Y axis value
-    uint8_t Emergency_Stop; // Emergency stop (1 = stop, 0 = go)
-    uint8_t Armed;          // Armed (1 = armed, 0 = disarmed)
-    uint8_t Button_A;       
-    uint8_t Button_B;
-    uint8_t Button_C;
-    uint8_t Button_D;
-    uint8_t Encoder_Pos;
-    uint8_t CTU_RSSI;           // Received Signal Strength Indicator
-    uint8_t Headlights_On;  // Headlights on (1 = on, 0 = off)
-    uint8_t Buzzer;         // Buzzer (1 = on, 0 = off)
+    float OBC_RSSI;
+
+    // CTU-specific fields (Telecommand, CTU -> OBC)
+    uint16_t Command;
+    uint16_t Joystick_X;    // Joystick X-axis raw ADC value
+    uint16_t Joystick_Y;    // Joystick Y-axis raw ADC value
+    uint16_t Encoder_Pos;
+    uint16_t CTU_RSSI;
+
+    bool Emergency_Stop;
+    bool Armed;
+    bool Headlights_On;
+    bool Buzzer;
+    bool Button_A;
+    bool Button_B;
+    bool Button_C;
+    bool Button_D;
 
 } xr4_system_context_t;
 
