2ac89ad9cb6cd262e26d3c6c6da56142886e8fd3
Salman Naveed <st186484@stud.uni-stuttgart.de>
2025-02-19 17:21:20 +0100

    feat: initial implementation of OBC main handler function

--- lib/SN_Handler/SN_Handler.cpp
+++ lib/SN_Handler/SN_Handler.cpp
@@ -5,9 +5,19 @@
 #include <SN_Logger.h>
 #include <SN_Joystick.h>
 #include <SN_XR_Board_Types.h>
+#include <SN_Motors.h>
 
 #include <stdint.h>
 #include <stdbool.h>
+
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_attr.h"
+
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
 
 extern xr4_system_context_t xr4_system_context;
 
@@ -39,14 +49,19 @@ bool get_flag(uint16_t flags, uint8_t bit_position) {
 #define JOYSTICK_X_AXIS 0
 #define JOYSTICK_Y_AXIS 1
 
-extern CTU_telecommand_message_t telecommand_message;
+extern bool TM_received_rdy_to_copy;
+
+extern telecommand_data_t CTU_out_telecommand_data;
 
 uint8_t EMERGENCY_STOP_ACTIVE = 0;
 uint8_t joystick_test_x = 0;
 uint8_t joystick_test_y = 0;
 
 #elif SN_XR4_BOARD_TYPE == SN_XR4_OBC_ESP32
-extern OBC_telemetry_message_t telemetry_message;
+
+extern telecommand_data_t OBC_in_telecommand_data;
+extern bool OBC_TC_received_data_ready;
+extern uint8_t OBC_TC_last_received_data_type;
 
 #endif
 
@@ -54,7 +69,7 @@ extern OBC_telemetry_message_t telemetry_message;
 
 void SN_CTU_MainHandler(){
   // CTU Handler
-  SN_CTU_compile_Telecommand_Message();
+  SN_CTU_compile_telecommand_data();
 
   SN_ESPNOW_SendTelecommand();
 
@@ -73,8 +88,6 @@ uint8_t SN_CTU_read_Emergency_Stop() {
 // ---------------- OBC Handler ----------------
 
 #elif SN_XR4_BOARD_TYPE == SN_XR4_OBC_ESP32
-
-
 
 void SN_OBC_TurnOnHeadlights() {
   xr4_system_context.Headlights_On = 1;
@@ -123,16 +136,49 @@ void SN_OBC_DrivingHandler() {
 
   joystick_values = SN_Joystick_OBC_MapADCValues(xr4_system_context.Joystick_X, xr4_system_context.Joystick_Y);
 
+  // if(joystick_values.joystick_x_mapped_val > 0) {
+  //   if(joystick_values.joystick_y_mapped_val == 0){
+  //     // Drive Forward
+  //     float speed = (float)joystick_values.joystick_x_mapped_val;
+  //     SN_Motors_DriveForward(speed);
+  //   }
+  //   else if(joystick_values.joystick_y_mapped_val > 0){
+  //     // Turn Right
+  //     float speed = (float)joystick_values.joystick_y_mapped_val;
+  //     SN_Motors_TurnRight(speed);
+  //   }
+  //   else if(joystick_values.joystick_y_mapped_val < 0){
+  //     // Turn Left
+  //     float speed = (float)joystick_values.joystick_y_mapped_val;
+  //     SN_Motors_TurnLeft(speed);
+  //   }
+
+  //   // Drive Forward
+  //   SN_Motors_DriveForward(speed);
+  // } 
+  // else if(joystick_values.joystick_x_mapped_val < 0) {
+  //   // Drive Backward
+  //   float speed = (float)joystick_values.joystick_x_mapped_val;
+  //   SN_Motors_DriveBackward(speed);
+  // } 
+  // else {
+  //   // Stop
+  //   SN_Motors_Stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
+  // }
+
 }
 
 
 void SN_OBC_MainHandler(){
   // OBC Handler
-  SN_OBC_ExecuteCommands();
 
+  SN_Telecommand_updateContext(OBC_in_telecommand_data);
+
+  SN_OBC_ExecuteCommands();
 
   SN_OBC_DrivingHandler();
 
+  SN_Telemetry_updateStruct(xr4_system_context);
 
   SN_ESPNOW_SendTelemetry();
 }
