54bbcef8caf082ae762393761d3dcbbf1def4068
Salman Naveed <st186484@stud.uni-stuttgart.de>
2025-03-19 21:58:00 +0100

    feat: defined CTU input switch pins

--- lib/SN_Common/SN_GPIO_Definitions.h
+++ lib/SN_Common/SN_GPIO_Definitions.h
@@ -14,6 +14,17 @@
 #define joystick_y_pin A3 // Joystick Y-axis input pin GPIO39 SVN
 
 
+// CTU Input Switches
+#define emergency_stop_pin 34 // Emergency Stop Switch input pin GPIO34
+#define armed_pin 35 // Armed Switch input pin GPIO35
+#define headlights_on_pin 32 // Headlights On Switch input pin GPIO32
+#define buzzer_pin 33 // Buzzer Switch input pin GPIO33
+#define button_a_pin 25 // Button A input pin GPIO25
+#define button_b_pin 26 // Button B input pin GPIO26
+#define button_c_pin 27 // Button C input pin GPIO27
+#define button_d_pin 14 // Button D input pin GPIO14
+
+
 #endif
 
 
