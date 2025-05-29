8b0b5beb01a743e2bd2687ab0d6405bbc2f6e770
Salman Naveed <st186484@stud.uni-stuttgart.de>
2025-05-29 21:32:07 +0200

    fix: added initializations for Joystick, input switches, LCD and Motors and GPS for CTU and OBC respectively

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,29 @@
 #include <SN_UART_SLIP.h>
 #include <SN_Handler.h>
 #include <SN_LCD.h>
+#include <SN_GPS.h>
+// #include <SN_Common.h>
+#include <SN_Joystick.h>
+#include <SN_Motors.h>
 
 extern bool espnow_init_success;
  
 void setup() {
+
   SN_UART_SLIP_Init();   // Init Serial Monitor
 
   #if SN_XR4_BOARD_TYPE == SN_XR4_CTU_ESP32
+    SN_Joystick_Init(); // Init Joystick
+    SN_Input_Init(); // Init CTU Inputs
     SN_LCD_Init();
   #endif
 
   SN_ESPNOW_Init();
 
-
+  #if SN_XR4_BOARD_TYPE == SN_XR4_OBC_ESP32
+    SN_Motors_Init(); // Init Motors
+    SN_GPS_Init(); // Init GPS
+  #endif
 }
  
 void loop() {
